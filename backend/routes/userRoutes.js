5f5ccb1e4c18358ca7813f05c0b18cb4d46fe5cd
Omkar <omkarsalunkhe3597@gmail.com>
2025-01-10 12:55:47 +0530

    routes added for login and register

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,5 +2,11 @@ const express=require('express')
 const router=express.Router()
 
 const {auth}=require('../middleware/auth')
+const {register,login}=require('../controllers/Auth')
+
+router.post('/register',register)
+router.post('/login',login)
+
+
 
 module.exports=router
