8befdeb6be2d61614127e3bb7513fd7b86e8cb96
Omkar <omkarsalunkhe3597@gmail.com>
2025-01-10 12:53:04 +0530

    login functionality added

--- backend/controllers/Auth.js
+++ backend/controllers/Auth.js
@@ -1,5 +1,6 @@
 const User=require('../models/User')
 const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
 
 const register=async(req,res)=>{
     try{
@@ -12,7 +13,7 @@ const register=async(req,res)=>{
                 message:"User is already present"
             })
         }
-        
+
         // create hash        
         let hashdpassword;
         try{
@@ -29,7 +30,7 @@ const register=async(req,res)=>{
         await User.create({username,password:hashdpassword})
         res.status(200).json({
             success:true,
-            message:"Registration successfull"
+            message:"Registration successfull!"
         })
     }
     catch(err){
@@ -39,3 +40,49 @@ const register=async(req,res)=>{
         })
     }
 }
+
+const login=async(req,res)=>{
+    try{
+        const {username,password}=req.body
+        const userData=User.find({username})
+
+        if(!userData)
+        {
+            res.status(404).json({
+                success:false,
+                message:"user not found"
+            })
+        }
+
+        const isPasswordMatch=await bcrypt.compare(password,userData.password)
+        if(isPasswordMatch)
+        {
+            const payload={
+                user_id:userData._id,
+                username:userData.username
+            }
+
+            const token=jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:'5h'})
+
+            res.status(200).json({
+                success:true,
+                token,
+                message:`Login successfull (Hi ${userData.username}`
+            })
+        }
+        else{
+            res.status(401).json({
+                success:false,
+                message:"Password not match"
+            })
+        }
+    }
+    catch(err){
+        res.satus(500).json({
+            success:false,
+            message:err.message
+        })
+    }
+}
+
+module.exports={register,login}
