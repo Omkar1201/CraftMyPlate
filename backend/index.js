7a152b50345fc50bf72de7367e6baf2d44fbe995
Omkar <omkarsalunkhe3597@gmail.com>
2025-01-11 12:36:50 +0530

    cors functionality added

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 const express=require('express')
 const app=express();
 const cookieParser = require('cookie-parser');
+const cors=require('cors')
+require('dotenv').config()
 
 const connnectDB=require('./config/database')
-require('dotenv').config()
+
+const corsOptions={
+    origin:[`${process.env.BASE_URL}`],
+    methods:"GET,POST,PUT,DELETE",
+    credentials:true,
+    allowedHeaders: 'Content-Type,Authorization'
+}
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors(corsOptions))
 
 const userRoutes=require('./routes/userRoutes')
 app.use('/api/v1/',userRoutes)
